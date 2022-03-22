adec3e5107a305c86f04ea658d564e352a6c2b0d
Manzar98 <manzarnouman@gmail.com>
2022-03-22 04:38:32 -0400

    updated

--- src/lib/components/DateInput.js
+++ src/lib/components/DateInput.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import DatePicker from "react-datepicker";
+
+/*
+1. simple react-datepicker
+
+Props : {
+    selectedDate,
+    setSelectedDate,
+    minDate,
+    maxDate,
+    filteredDate
+}
+*/
+
 const DateInput = (props) => {
     return (
-        <div>Date Input</div>
+        <DatePicker
+            className="form-control"
+            selected={props?.selectedDate}
+            onChange={(date) => { props?.setSelectedDate(date) }}
+            minDate={props?.minDate}
+            maxDate={props?.maxDate}
+            filterDate={props?.filteredDate}
+        />
     )
 }
 export default DateInput;
--- src/lib/components/MultiSelect.js
+++ src/lib/components/MultiSelect.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import Select from 'react-select';
+
+
+/*
+1. simple react-datepicker
+
+Props : {
+    defaultValue,
+    require,
+    isMulti,
+    options
+}
+*/
+
 const MultiSelectDropDown = (props) => {
+
+    
+	const [isMultiSelectOpen, setIsMultiSelectOpen] = useState(false);
+	const [selectedMultiOption, setSelectedMultiOption] = useState('');
     return (
-        <div>MultiSelectDropDown</div>
+        <div
+					className={`select ${
+						(props?.defaultValue && props?.defaultValue.length !== 0) || isMultiSelectOpen || selectedMultiOption
+							? 'select-open'
+							: 'select-close'
+					}`}
+				>
+                    
+					<label htmlFor='name'>
+						{props.label} {props.require && <em className='text-red-400'>*</em>}
+					</label>
+					<Select
+						isMulti={props.isMulti}
+						isClearable={false}
+						classNamePrefix={'my-custom-react-select'}
+						placeholder=''
+						value={props.defaultValue || selectedMultiOption}
+						onChange={(selectedMultiOption) => {
+							console.log(selectedMultiOption);
+							setSelectedMultiOption(selectedMultiOption);
+							props.setValue(selectedMultiOption);
+						}}
+						options={props.options}
+						required
+						spellCheck='false'
+						onMenuOpen={() => {
+							setIsMultiSelectOpen(true);
+						}}
+						onMenuClose={() => {
+							setIsMultiSelectOpen(false);
+						}}
+					/>
+				</div>
     )
 }
 export default MultiSelectDropDown;
